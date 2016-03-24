aee47fa6e28fe458948c137e04540a052b1a152a
Scott Mansfield <sgmansf@gmail.com>
2016-03-23 21:49:06 -0700

    Giving the server 10 seconds to start before integ tests run

--- integ_test.go
+++ integ_test.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
+	"net"
 	"net/http"
 	"os"
 	"testing"
+	"time"
 )
 
 func TestMain(m *testing.M) {
@@ -15,6 +18,24 @@ func TestMain(m *testing.M) {
 	os.Args = append(os.Args, "-test.v")
 
 	go main()
+
+	// make sure the server is up before running tests
+	tries := 10
+	for i := 0; i < tries; i++ {
+		if conn, err := net.Dial("tcp", "localhost:9001"); err == nil {
+			conn.Close()
+			fmt.Println("Server up!")
+			break
+		}
+		if i < tries-1 {
+			fmt.Printf("Server not up yet. Try %d\n", i)
+		} else {
+			fmt.Printf("Server not up after %d tries. Failing tests.\n", tries)
+			os.Exit(-1)
+		}
+		<-time.After(1 * time.Second)
+	}
+
 	os.Exit(m.Run())
 }
 
